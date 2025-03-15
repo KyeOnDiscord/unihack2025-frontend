2dfb514400ecc66ea12f44dcfbaa17a0418e5765
Jason Yi <jasonyi1413@gmail.com>
2025-03-15 23:17:48 +1100

    Added login toastify

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import * as config from "../config";
 import * as UserService from "./services/user";
+import { toast } from 'react-toastify';
 
 console.log(UserService);
 export default function LoginForm() {
@@ -23,7 +24,13 @@ export default function LoginForm() {
     // Handle form submission (e.g., send data to API)
     // console.log(formData);
     // console.log(config.API_URL);
-    await UserService.LoginUser(formData.email, formData.password);
+    toast.promise(UserService.LoginUser(formData.email, formData.password),
+      { 
+        pending: 'Logging in...',
+        success: { render: 'Logged in! ✅', delay: 100 },
+        error: { render: 'There was a problem while logging in. 😭', delay: 100 },
+      }
+    );
   };
 
   return (
