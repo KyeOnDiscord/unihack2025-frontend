25814f269e59b0962bcc46608d2538f380c85a63
Kye <promasterboy@outlook.com>
2025-03-16 12:54:29 +1100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,3 @@
-API_URL = null;
-
-if (process.env.NODE_ENV === "development") {
-  API_URL = "http://127.0.0.1:8000";
-} else if (process.env.NODE_ENV === "production") {
-  API_URL = "http://209.38.93.77:8000";
-}
-module.exports = {
-  API_URL,
-  INTERFACE_API_KEY:
-    "UNIHACK-UF4lN4R5ATOl73DrOaoOqB0E9HoefL9lfIGBw50naUDKIKSL8Afl57WIZA5tGEEtBiDm3ixHnKHGiNCkMYEfRBZvLVQ9NDPcXmgNlQawSCEG9pontG4dyfPGidhht30u",
-};
+export const API_URL = "http://209.38.93.77:8000";
+export const INTERFACE_API_KEY =
+  "UNIHACK-UF4lN4R5ATOl73DrOaoOqB0E9HoefL9lfIGBw50naUDKIKSL8Afl57WIZA5tGEEtBiDm3ixHnKHGiNCkMYEfRBZvLVQ9NDPcXmgNlQawSCEG9pontG4dyfPGidhht30u";
