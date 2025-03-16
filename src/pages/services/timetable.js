7f25cb32118600fc26b6b1ff2fd2104b12992927
bibkin-maks <maksimBibki@gmail.com>
2025-03-16 12:24:34 +1100

    Free slots popups+ updated apiurl

--- src/pages/services/timetable.js
+++ src/pages/services/timetable.js
@@ -5,10 +5,165 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
+export function PeopleTeam16(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <g fill="none">
+        <path
+          fill="url(#fluentColorPeopleTeam160)"
+          d="M12.562 13A2.5 2.5 0 0 0 15 10.5V8a1 1 0 0 0-1-1h-3a1 1 0 0 0-1 1v2.5a2.5 2.5 0 0 0 2.562 2.5"
+        ></path>
+        <path
+          fill="url(#fluentColorPeopleTeam167)"
+          fillOpacity=".5"
+          d="M12.562 13A2.5 2.5 0 0 0 15 10.5V8a1 1 0 0 0-1-1h-3a1 1 0 0 0-1 1v2.5a2.5 2.5 0 0 0 2.562 2.5"
+        ></path>
+        <path
+          fill="url(#fluentColorPeopleTeam161)"
+          d="M3.562 13A2.5 2.5 0 0 0 6 10.5V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2.5A2.5 2.5 0 0 0 3.562 13"
+        ></path>
+        <path
+          fill="url(#fluentColorPeopleTeam168)"
+          fillOpacity=".5"
+          d="M3.562 13A2.5 2.5 0 0 0 6 10.5V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2.5A2.5 2.5 0 0 0 3.562 13"
+        ></path>
+        <path
+          fill="url(#fluentColorPeopleTeam162)"
+          d="M5 7.993A1 1 0 0 1 6 7h4a1 1 0 0 1 1 1v3a3 3 0 0 1-.146.927A3.001 3.001 0 0 1 5 11z"
+        ></path>
+        <path
+          fill="url(#fluentColorPeopleTeam163)"
+          d="M5 7.993A1 1 0 0 1 6 7h4a1 1 0 0 1 1 1v3a3 3 0 0 1-.146.927A3.001 3.001 0 0 1 5 11z"
+        ></path>
+        <path
+          fill="url(#fluentColorPeopleTeam164)"
+          d="M12.5 3a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0-3"
+        ></path>
+        <path
+          fill="url(#fluentColorPeopleTeam165)"
+          d="M3.5 3a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0-3"
+        ></path>
+        <path
+          fill="url(#fluentColorPeopleTeam166)"
+          d="M8 2.002a1.998 1.998 0 1 0 0 3.996a1.998 1.998 0 0 0 0-3.996"
+        ></path>
+        <defs>
+          <linearGradient
+            id="fluentColorPeopleTeam160"
+            x1="11.189"
+            x2="14.454"
+            y1="7.797"
+            y2="12.142"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".125" stopColor="#7A41DC"></stop>
+            <stop offset="1" stopColor="#5B2AB5"></stop>
+          </linearGradient>
+          <linearGradient
+            id="fluentColorPeopleTeam161"
+            x1="2.189"
+            x2="5.454"
+            y1="7.797"
+            y2="12.142"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".125" stopColor="#9C6CFE"></stop>
+            <stop offset="1" stopColor="#7A41DC"></stop>
+          </linearGradient>
+          <linearGradient
+            id="fluentColorPeopleTeam162"
+            x1="6.427"
+            x2="10.205"
+            y1="7.93"
+            y2="13.101"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".125" stopColor="#BD96FF"></stop>
+            <stop offset="1" stopColor="#9C6CFE"></stop>
+          </linearGradient>
+          <linearGradient
+            id="fluentColorPeopleTeam163"
+            x1="8"
+            x2="13.697"
+            y1="6.166"
+            y2="15.29"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#885EDB" stopOpacity="0"></stop>
+            <stop offset="1" stopColor="#E362F8"></stop>
+          </linearGradient>
+          <linearGradient
+            id="fluentColorPeopleTeam164"
+            x1="11.713"
+            x2="13.242"
+            y1="3.399"
+            y2="5.84"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".125" stopColor="#7A41DC"></stop>
+            <stop offset="1" stopColor="#5B2AB5"></stop>
+          </linearGradient>
+          <linearGradient
+            id="fluentColorPeopleTeam165"
+            x1="2.713"
+            x2="4.242"
+            y1="3.399"
+            y2="5.84"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".125" stopColor="#9C6CFE"></stop>
+            <stop offset="1" stopColor="#7A41DC"></stop>
+          </linearGradient>
+          <linearGradient
+            id="fluentColorPeopleTeam166"
+            x1="6.952"
+            x2="8.989"
+            y1="2.533"
+            y2="5.785"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".125" stopColor="#BD96FF"></stop>
+            <stop offset="1" stopColor="#9C6CFE"></stop>
+          </linearGradient>
+          <radialGradient
+            id="fluentColorPeopleTeam167"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientTransform="matrix(4.02365 0 0 8.1916 9.214 9.86)"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".433" stopColor="#3B148A"></stop>
+            <stop offset="1" stopColor="#3B148A" stopOpacity="0"></stop>
+          </radialGradient>
+          <radialGradient
+            id="fluentColorPeopleTeam168"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientTransform="matrix(-4.45284 0 0 -9.06537 7.62 9.86)"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".433" stopColor="#3B148A"></stop>
+            <stop offset="1" stopColor="#3B148A" stopOpacity="0"></stop>
+          </radialGradient>
+        </defs>
+      </g>
+    </svg>
+  )
+}
+
+
 function GroupCalendar({ free_times }) {
   const [events, setEvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState(null);
-  const [view, setView] = useState("month");
+  const [view, setView] = useState("week");
 
   React.useEffect(() => {
     const formattedEvents = free_times.map((event, index) => {
@@ -128,7 +283,7 @@ function GroupCalendar({ free_times }) {
           padding: "20px",
           boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
           width: "80vw",
-          maxWidth: "1200px",
+          maxWidth: "75vw",
         }}
       >
         {/* Calendar Component */}
@@ -145,13 +300,14 @@ function GroupCalendar({ free_times }) {
           components={{
             toolbar: CustomToolbar,
           }}
-          style={{ height: 650, width: "75vw" }}
+          className="w-full"
+          style={{ height: 650 }}
         />
       </div>
       {/* Event Details Modal */}
       {selectedEvent && (
         <div
-          className="bg-gradient-to-r from-teal-200 to-teal-500 text-white"
+          className=" bg-white text-black shadow-[0px_10px_11px_10px_rgba(0,0,0,1)]"
           style={{
             position: "fixed",
             top: "50%",
@@ -163,21 +319,28 @@ function GroupCalendar({ free_times }) {
             zIndex: 1000,
           }}
         >
+          <div className="w-full flex justify-center">
+           <PeopleTeam16 height="100px" width="90px"></PeopleTeam16>
+          </div>
           <h3 className="text-lg font-semibold w-full text-center">
             Congratulations!
           </h3>
+          
+         
           <p className="text-sm text-gray-600">{selectedEvent.description}</p>
           <p className="text-sm text-gray-600">
             {moment(selectedEvent.start).format("LLL")} -{" "}
             {moment(selectedEvent.end).format("LLL")}
           </p>
+          <div className="w-full flex justify-center">
           <button
             type="submit"
             onClick={() => setSelectedEvent(null)}
-            className="  mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
+            className="bg-[#232324;] shadow-[0px_10px_11px_-10px_rgba(0,0,0,1)] mt-4 px-4 py-2  text-white rounded-md"
           >
             Close
           </button>
+          </div>
         </div>
       )}
 
